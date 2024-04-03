a5411c73354911e864ce21bd408183abe7834f15
Hariharanreddy <animatrix536@gmail.com>
2024-04-03 11:20:54 +0530

    first commit

---
+++ src/app/dummy/page.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <div>page</div>
+      <div>page</div>
+      <div>page</div>
+      <div>page</div>
+      <div>page</div>
+      <div>page</div>
+      <div>page</div>
+      <div>page</div>
+      <div>page</div>
+      <div>page</div>
+      <div>page</div>
+      <div>page</div>
+    </>
+  );
+};
+
+export default page;
